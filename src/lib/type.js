b87203a56fe6bc03c8746c4b0becc9305fb28a44
gonior <arthemuss14@gmail.com>
2021-08-03 09:29:07 +0700

    Remove String from keyword

--- src/lib/type.js
+++ src/lib/type.js
@@ -3,5 +3,5 @@ export const TYPE = {
     symbols: ['"', '\n', "'"],
     other_symbols: ['//', '/*', '*/'],
     operator: ["*", '+', '/', '=', '%','<', '>',, "<=", '>=', '!=', "=="],
-    keywords: ['abstract', 'boolean', 'break', 'byte', 'case', 'catch', 'char', 'class', 'do', 'double', 'else', 'enum', 'exports', 'extends', 'final', 'finally', 'float', 'goto', 'implements', 'import', 'instanceof', 'long', 'new', 'package', 'private', 'protected', 'requires', 'return', 'short', 'super', 'switch', 'this', 'throw', 'throws', 'try', 'while', 'public', 'void', 'static', 'String', 'int', 'if', 'for'],
+    keywords: ['abstract', 'boolean', 'break', 'byte', 'case', 'catch', 'char', 'class', 'do', 'double', 'else', 'enum', 'exports', 'extends', 'final', 'finally', 'float', 'goto', 'implements', 'import', 'instanceof', 'long', 'new', 'package', 'private', 'protected', 'requires', 'return', 'short', 'super', 'switch', 'this', 'throw', 'throws', 'try', 'while', 'public', 'void', 'static', 'int', 'if', 'for'],
 };   
